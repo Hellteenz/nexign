a4275f9fad919d011043234c36a9fc555e8fd6ff
Kristina <kristina.zezina.nn@gmail.com>
2024-03-24 15:30:40 +0300

    commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
